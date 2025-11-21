3252c17411a0b687ce916f304ce623ee8ace38e4
TungCorn <tung01012005s@gmail.com>
2025-11-21 23:24:02 +0700

    Add ProductController and update routes for product management

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -11,7 +12,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all()->OrderBy('created_at', 'desc');
+        return view('products.index', compact('products'));
     }
 
     /**
